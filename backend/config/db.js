34711fc30fd26ded56aea7903e30ebf1b9b592d6
darshanparekh77 <parekhdarshan07@gmail.com>
2024-10-20 15:53:31 +0530

    change3

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const uri = process.env.MONGO_URI; // Make sure this is correct
+        if (!uri) {
+            throw new Error("MongoDB URI is undefined. Check your .env file.");
+        }
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
